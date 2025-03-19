f7a56bdb16cda42abce953acd910a544afae506b
Mohit Kumar <52292512+Mohit0233@users.noreply.github.com>
2025-03-19 21:22:38 +0530

    Refactor main.tf to use local variable for instance types and add script.sh for Doormat login

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,20 +13,25 @@ provider "aws" {
   region = var.aws_region
 }
 
+locals {
+  instance_type_list = tolist(var.instance_types)  # Ensure it's a list
+}
+
 resource "aws_instance" "github_runners" {
-  count = length(var.instance_types)
-  ami = "ami-09773b29dffbef1f2"
-  instance_type = var.instance_types[count.index]
-  key_name      = "aws-test-github-self-hosted-runner"
+  count = length(local.instance_type_list)
+
+  ami             = "ami-09773b29dffbef1f2"
+  instance_type   = local.instance_type_list[count.index]  # Extract single value
+  key_name        = "aws-test-github-self-hosted-runner"
   vpc_security_group_ids = [var.security_group]
 
   tags = {
-    Name = "github-ephemeral-runner-${var.instance_types[count.index]}"
+    Name = "github-ephemeral-runner-${local.instance_type_list[count.index]}"
   }
 
   user_data = base64encode(templatefile("${path.module}/install-runner.sh.tpl", {
     RUNNER_TOKEN  = var.runner_token
-    INSTANCE_TYPE = var.instance_types[count.index]
+    INSTANCE_TYPE = local.instance_type_list[count.index]  # Now Terraform gets only a single value per instance
     GITHUB_REPO   = var.github_repo
   }))
 }
